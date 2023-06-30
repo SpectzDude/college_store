14c9eab61ee6b7aaf0c919cf96911e3ea43b2a4a
Ajmal Nasumudeen <ajmal.nasumudeen@treeswise.com>
2023-06-30 18:46:48 +0530

    Added proper closing tag

--- client/src/modules/auth/components/Login.jsx
+++ client/src/modules/auth/components/Login.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
 const Login = () => {
-    return<><div className="loginbox">
-    <img src="user.png" className="avatar">
-    <h1>Login Here</h1>
-    <form>
-        <p>User Name</p>
-        <input type="text" name="" placeholder="Enter User Name">
-        <p>Password</p>
-        <input type="password" name="" placeholder="Enter Password">
-        <input type="submit" name="" value="Login">
-        <a href="#">Forgot Password</a><br>
-        <a href="#">Don,t Have An Account?</a>
-    </form>
-   </div>
- </>;
+    return <><div className="loginbox">
+        <img src="user.png" className="avatar" />
+        <h1>Login Here</h1>
+        <form>
+            <p>User Name</p>
+            <input type="text" name="" placeholder="Enter User Name" />
+            <p>Password</p>
+            <input type="password" name="" placeholder="Enter Password" />
+            <input type="submit" name="" value="Login" />
+            <a href="#">Forgot Password</a><br />
+            <a href="#">Don,t Have An Account?</a>
+        </form>
+    </div>
+    </>;
 };
 
 export default Login;
